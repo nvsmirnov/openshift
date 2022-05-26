a0a504d63742b25d9b34e263e3317a25ba0d8668
nvsmirnov <50905641+nvsmirnov@users.noreply.github.com>
2022-05-26 16:23:14 +0300

    Update vsphere-perms-govc-env.sh

--- vspere-prepare/vsphere-perms-govc-env.sh
+++ vspere-prepare/vsphere-perms-govc-env.sh
@@ -1,4 +1,8 @@
-export GOVC_URL=vsphere.hq.metib.ru
+# use these parameters to connect to VSphere
+# give a user with Admin privileges
+# this user needed only once, when creating permissions, then it should be blocked/removed from VCenter.
+# as for me, I created temporary user for this
+export GOVC_URL=vsphere.name.tld
 export GOVC_USERNAME='vsphere.local\k8s-okd-preinstaller'
 export GOVC_PASSWORD='XXXX'
 export GOVC_INSECURE=1
